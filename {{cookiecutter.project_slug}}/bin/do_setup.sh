d6f7bc3a59a001667b2d2a18cb0be7b902383a60
Raffaele Colace <raffaele@20tab.com>
2020-02-18 14:58:15 +0100

    useless access token parameter removed

--- {{cookiecutter.project_slug}}/bin/do_setup.sh
+++ {{cookiecutter.project_slug}}/bin/do_setup.sh
@@ -3,8 +3,7 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -a do_access_token -c cluster_name -r region"
-   echo -e "\t-a Access token generated on DigitalOcean API site"
+   echo "Usage: $0 -c cluster_name -r region"
    echo -e "\t-c Created cluster's name"
    echo -e "\t-r Region used during cluster creation"
    exit 1 # Exit script after printing help
@@ -13,7 +12,6 @@ helpFunction()
 while getopts "a:c:r:" opt
 do
    case "$opt" in
-      a ) parameterA="$OPTARG" ;;
       c ) parameterC="$OPTARG" ;;
       r ) parameterR="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
@@ -21,13 +19,13 @@ do
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$parameterA" ] || [ -z "$parameterC" ] || [ -z "$parameterR" ]
+if [ -z "$parameterC" ] || [ -z "$parameterR" ]
 then
-   echo "Some or all of the parameters are empty";
+   echo "Some required parameters are empty";
    helpFunction
 fi
 
-doctl auth init -t $parameterA
+doctl auth init
 doctl kubernetes cluster kubeconfig save $parameterC
 kubectl config use-context $parameterR-$parameterC
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.28.0/deploy/static/mandatory.yaml
