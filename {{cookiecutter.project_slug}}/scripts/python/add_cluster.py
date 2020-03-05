0849af43eedad866a5ab00a37da34c25364c488a
Raffaele Colace <raffaele@20tab.com>
2020-03-05 11:41:48 +0100

    add cluster procedure improved

--- {{cookiecutter.project_slug}}/scripts/python/add_cluster.py
+++ {{cookiecutter.project_slug}}/scripts/python/add_cluster.py
@@ -1,6 +1,7 @@
 """Define Cluster class and utilities."""
 
 import base64
+import os
 
 from gitlab_sync import GitlabSync
 from kubernetes import Cluster
@@ -21,18 +22,35 @@ def main():
     certificate_str = credentials["certificate_authority_data"]
     certificate_bytes = base64.b64decode(certificate_str)
     certificate = certificate_bytes.decode()
+    token = None
+    with open("do_token.yaml", "r") as f:
+        for line in f.readlines():
+            couple = line.split(":")
+            try:
+                key = couple[0].strip()
+                value = couple[1].strip()
+                if key == "token":
+                    token = value
+            except KeyError as e:
+                continue
+            except IndexError as e:
+                continue
 
-    cluster = group.clusters.create(
-        {
-            "name": cluster_name,
-            "platform_kubernetes_attributes": {
-                "api_url": credentials["server"],
-                "token": credentials["token"],
-                "ca_cert": certificate,
-            },
-            "managed": False,
-        }
-    )
+    if token:
+        cluster = group.clusters.create(
+            {
+                "name": cluster_name,
+                "platform_kubernetes_attributes": {
+                    "api_url": credentials["server"],
+                    "token": token,
+                    "ca_cert": certificate,
+                },
+                "managed": False,
+            }
+        )
+        os.remove("do_token.yaml")
+    else:
+        print("Token not found. Please check do_token.yaml file into the projects' root.")
 
 
 if __name__ == "__main__":
