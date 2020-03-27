bdaa537411be4457b9c57af005cc59f2a661890a
Carmelo Catalfamo <carmelo.catalfamo92@gmail.com>
2020-03-27 17:10:17 +0100

    ingress-nginx logs. Rename project folder.

--- {{cookiecutter.project_slug}}/scripts/add_cluster.sh
+++ {{cookiecutter.project_slug}}/scripts/add_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl apply -f k8s/gitlab-admin-service-account.yaml
+kubectl apply -f k8s/cluster/gitlab-admin-service-account.yaml
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep gitlab-admin | awk '{print $1}') > do_token.yaml
 python ./scripts/python/add_cluster.py
