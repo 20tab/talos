a79467b92912c660294c8d0183b31965f70b81c3
Paolo Melchiorre <paolo@20tab.com>
2020-05-06 09:53:06 +0200

    🚜️ Refactor pre gen project hook

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,41 +1,29 @@
-"""Define hooks to be run before project generation."""
+#!/usr/bin/env python
+"""
+Define hooks to be run before project generation.
+
+NOTE: OrderedDict is used by cookiecutter during jinja template render.
+"""
 
 import json
 import os
-import re
-import unicodedata
+import sys
 from collections import OrderedDict  # noqa
+from pathlib import Path
+
+from slugify import slugify
 
 try:
     import gitlab  # noqa
 except ModuleNotFoundError:  # pragma: no cover
     pass
 
-# OrderedDict is used by cookiecutter during jinja template render
-
-
-def slugify(value):
-    """
-    Transofrm text into slug.
-
-    Convert to ASCII.
-    Convert spaces to hyphens.
-    Remove characters that aren't alphanumerics, underscores, or hyphens.
-    Convert to lowercase.
-    Also strip leading and trailing whitespace.
-    """
-    value = str(value)
-    value = (
-        unicodedata.normalize("NFKD", str(value))
-        .encode("ascii", "ignore")
-        .decode("ascii")
-    )
-    value = re.sub(r"[^\w\s-]", "", value.casefold()).strip()
-    return re.sub(r"[-\s]+", "-", value)
-
 
 class MainProcess:
     """Main process class."""
+
+    URL = "https://gitlab.com"
+    TOKEN = "GITLAB_PRIVATE_TOKEN"
 
     def __init__(self, *args, **kwargs):
         """Create a main process instance with chosen parameters."""
@@ -44,10 +32,18 @@ class MainProcess:
         self.group_slug = self.project_slug
         self.use_gitlab = "{{ cookiecutter.use_gitlab }}" == "Yes"
         if self.use_gitlab:
-            self.gl = gitlab.Gitlab(
-                "https://gitlab.com", private_token=os.environ["GITLAB_PRIVATE_TOKEN"]
-            )
-            self.gl.auth()
+            try:
+                private_token = os.environ[self.TOKEN]
+            except KeyError:
+                sys.exit(f"The environment variable '{self.TOKEN}' is missing.")
+            try:
+                self.gl = gitlab.Gitlab(self.URL, private_token=private_token)
+            except NameError:
+                sys.exit("The 'python-gitlab' package is missing.")
+            try:
+                self.gl.auth()
+            except gitlab.exceptions.GitlabAuthenticationError:
+                sys.exit(f"The environment variable '{self.TOKEN}' is not correct.")
 
     def is_group_slug_available(self, group_slug):
         """Tell if group name is available."""
@@ -55,10 +51,7 @@ class MainProcess:
             if p.path == group_slug:
                 return False
         for u in self.gl.users.list(username=group_slug):
-            if (
-                u.web_url.replace("https://gitlab.com/", "").casefold()
-                == group_slug.casefold()
-            ):
+            if u.web_url.replace(self.URL, "").casefold() == group_slug.casefold():
                 return False
         return True
 
@@ -68,16 +61,16 @@ class MainProcess:
             input(f"Choose the gitlab group path slug [{self.group_slug}]: ")
             or self.group_slug
         )
-        self.group_slug = slugify(group_slug)
+        self.group_slug = slugify(group_slug, separator="")
         while len(self.group_slug) > 30:
-            self.group_slug = slugify(
-                input("Please choose a name shorter than 30 characters: ")
-            )
+            group_slug = input("Please choose a name shorter than 30 characters: ")
+            self.group_slug = slugify(group_slug, separator="")
         while not self.is_group_slug_available(self.group_slug):
-            self.group_slug = input(
+            group_slug = input(
                 f"A Gitlab group named '{self.group_slug}' already exists. "
                 "Please choose a different name and try again: "
             )
+            self.group_slug = slugify(group_slug, separator="")
 
     def run(self):
         """Run main process."""
@@ -87,9 +80,7 @@ class MainProcess:
         if self.use_gitlab:
             self.set_gitlab_group_slug()
             configuration["gitlab_group_slug"] = self.group_slug
-
-        with open("cookiecutter.json", "w+") as f:
-            f.write(json.dumps(configuration, indent=2))
+        Path("cookiecutter.json").write_text(json.dumps(configuration, indent=2))
 
 
 if __name__ == "__main__":
