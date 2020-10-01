ddd7f4559dda899cdf93b6ec7b57ba43c4f4cb9f
Paolo Melchiorre <paolo@melchiorre.org>
2020-10-01 15:17:47 +0200

    ⬆️ Update kubernetes ingress

--- {{cookiecutter.project_slug}}/scripts/do_setup.sh
+++ {{cookiecutter.project_slug}}/scripts/do_setup.sh
@@ -2,7 +2,7 @@
 
 doctl auth init
 python3 ./scripts/python/do_setup.py
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/do/deploy.yaml
+kubectl apply -f kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/do/deploy.yaml
 
 while true; do
    read -p "Do you want to add cluster to gitlab group?[Y/n]: "  yn
