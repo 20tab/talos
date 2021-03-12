601796fe8dc90e4ef22172c99efb62fb8ccd73d8
Paolo Melchiorre <paolo@melchiorre.org>
2021-03-12 12:03:10 +0100

    Activate bash strict mode

--- {{cookiecutter.project_slug}}/scripts/add_cluster.sh
+++ {{cookiecutter.project_slug}}/scripts/add_cluster.sh
@@ -2,7 +2,7 @@
 
 # Bash "strict mode", to help catch problems and bugs in the shell script
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
-# set -euo pipefail
+set -euo pipefail
 
 kubectl apply -f k8s/cluster/gitlab-admin-service-account.yaml
 python3 ./scripts/python/add_cluster.py
