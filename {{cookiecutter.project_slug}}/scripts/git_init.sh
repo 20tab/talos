4a306478b812ed320a0400d5aebbb258292b0a57
Paolo Melchiorre <paolo@20tab.com>
2021-12-01 19:00:06 +0100

    Fix #163 Broken git initialization

--- {{cookiecutter.project_slug}}/scripts/git_init.sh
+++ {{cookiecutter.project_slug}}/scripts/git_init.sh
@@ -4,10 +4,10 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 
-git init
-git add .
-git commit -m'Initial commit'
+git init --initial-branch=main
 git remote add origin $1
-git push -u origin master -o ci.skip
+git add .
+git commit -m "Initial commit"
+git push -u origin main -o ci.skip
 git checkout -b develop
 git push -u origin develop -o ci.skip
