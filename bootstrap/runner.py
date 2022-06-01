7a86190345fc9da27f251eb9a7cd660e2fa6be59
Filippo Morelli <filippo@20tab.com>
2022-06-01 19:03:54 +0000

    Fix subrepo Vault support

--- bootstrap/runner.py
+++ bootstrap/runner.py
@@ -365,12 +365,14 @@ class Runner:
             "terraform_backend": self.terraform_backend,
             "terraform_cloud_admin_email": self.terraform_cloud_admin_email,
             "terraform_cloud_hostname": self.terraform_cloud_hostname,
-            "terraform_cloud_organization": self.terraform_cloud_organization,
             "terraform_cloud_organization_create": False,
+            "terraform_cloud_organization": self.terraform_cloud_organization,
             "terraform_cloud_token": self.terraform_cloud_token,
             "terraform_dir": str(self.terraform_dir.resolve()),
             "uid": self.uid,
             "use_redis": self.use_redis,
+            "vault_address": self.vault_address,
+            "vault_token": self.vault_token,
             **kwargs,
         }
         subprocess.run(
