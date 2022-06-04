1ae11439ee273f3a326ed49bdfac3968c4bb06a7
Filippo Morelli <filippo@20tab.com>
2022-06-04 03:33:33 +0200

    Fix basic auth enabled

--- {{cookiecutter.project_dirname}}/terraform/environment/digitalocean-k8s/main.tf
+++ {{cookiecutter.project_dirname}}/terraform/environment/digitalocean-k8s/main.tf
@@ -194,7 +194,6 @@ module "metrics" {
 
   project_domain = var.project_domain
 
-  basic_auth_enabled  = var.basic_auth_enabled
   basic_auth_username = var.basic_auth_username
   basic_auth_password = var.basic_auth_password
 
