9ceaee317e0dc99b7a6fd03927d58efd88710142
Filippo Morelli <filippo@20tab.com>
2022-06-30 16:39:23 +0000

    Fetch Vault secrets

---
+++ {{cookiecutter.project_dirname}}/scripts/terraform-cloud.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+export TF_CLI_CONFIG_FILE="${TF_ROOT}/cloud.tfc"
+cat << EOF > ${TF_CLI_CONFIG_FILE}
+{
+  "credentials": {
+    "app.terraform.io": {
+      "token": "${TFC_TOKEN}"
+    }
+  }
+}
+EOF
