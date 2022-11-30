c0d6d2d6151e16b5a4f0e32460680a79c1c2024d
Filippo Morelli <filippo@20tab.com>
2022-11-30 11:15:44 +0100

    Add silent flag to curl

--- {{cookiecutter.project_dirname}}/scripts/deploy/vault.sh
+++ {{cookiecutter.project_dirname}}/scripts/deploy/vault.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
 
-export VAULT_TOKEN=`curl --request POST --data "role=${VAULT_ROLE}" --data "jwt=${CI_JOB_JWT_V2}" ${VAULT_ADDR%/}/v1/auth/gitlab-jwt/login | jq -r .auth.client_token`
+export VAULT_TOKEN=`curl --silent --request POST --data "role=${VAULT_ROLE}" --data "jwt=${CI_JOB_JWT_V2}" ${VAULT_ADDR%/}/v1/auth/gitlab-jwt/login | jq -r .auth.client_token`
 
 for secret_path in ${VAULT_SECRETS}
 do
     secret_var_file=${TERRAFORM_VARS_DIR}/`echo ${secret_path} | tr / -`.json
-    curl --header "X-Vault-Token: ${VAULT_TOKEN}" ${VAULT_ADDR%/}/v1/${PROJECT_SLUG}/${VAULT_SECRETS_PREFIX}/${secret_path} | jq -r ".data // {}" > ${secret_var_file}
+    curl --silent --header "X-Vault-Token: ${VAULT_TOKEN}" ${VAULT_ADDR%/}/v1/${PROJECT_SLUG}/${VAULT_SECRETS_PREFIX}/${secret_path} | jq -r ".data // {}" > ${secret_var_file}
     var_files="${var_files} -var-file=${secret_var_file}"
 done
 
 if [ "${TERRAFORM_BACKEND}" == "terraform-cloud" ]; then
-    export TFC_TOKEN=`curl --header "X-Vault-Token: ${VAULT_TOKEN}" ${VAULT_ADDR%/}/v1/${PROJECT_SLUG}-tfc/creds/default | jq -r .data.token `
+    export TFC_TOKEN=`curl --silent --header "X-Vault-Token: ${VAULT_TOKEN}" ${VAULT_ADDR%/}/v1/${PROJECT_SLUG}-tfc/creds/default | jq -r .data.token `
 fi
