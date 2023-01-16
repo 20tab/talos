ffea48d6bb6e5e9129dac80a9649b0a54cbd3b16
Matteo Vitali <matteo@20tab.com>
2023-01-16 16:31:29 +0100

    Improve loki configurations

--- {{cookiecutter.project_dirname}}/terraform/environment/modules/kubernetes/monitoring/main.tf
+++ {{cookiecutter.project_dirname}}/terraform/environment/modules/kubernetes/monitoring/main.tf
@@ -41,7 +41,10 @@ resource "helm_release" "loki" {
   chart      = "loki-stack"
   version    = "2.6.4"
 
-  values = local.s3_storage_enabled ? [file("${path.module}/loki/values.yaml")] : []
+  values = concat(
+    [file("${path.module}/loki/values.yaml")], 
+    local.s3_storage_enabled ? [file("${path.module}/loki/s3_storage.yaml")] : [file("${path.module}/loki/pvc_storage.yaml")]
+  )
 
   dynamic "set" {
     for_each = local.s3_storage_enabled ? {
@@ -50,14 +53,7 @@ resource "helm_release" "loki" {
       "loki.config.storage_config.aws.endpoint"           = var.s3_host
       "loki.config.storage_config.aws.region"             = var.s3_region
       "loki.config.storage_config.aws.secret_access_key"  = var.s3_secret_key
-    } : {
-      "loki.config.chunk_store_config.max_look_back_period" = "4400h"
-      "loki.config.limits_config.max_query_length"          = "2200h"
-      "loki.config.table_manager.retention_deletes_enabled" = "true"
-      "loki.config.table_manager.retention_period"          = "4400h"
-      "loki.persistence.enabled"                            = "true"
-      "loki.persistence.size"                               = "10Gi"
-    }
+    } : {}
     content {
       name  = set.key
       value = set.value
