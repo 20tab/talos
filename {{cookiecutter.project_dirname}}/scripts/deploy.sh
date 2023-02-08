8efcced95dee168cca5b2f623514ceca71b80ee0
Filippo Morelli <filippo@20tab.com>
2023-02-08 18:26:44 +0100

    TG-921 TG-925 Refactoring sh scripts

--- {{cookiecutter.project_dirname}}/scripts/deploy.sh
+++ {{cookiecutter.project_dirname}}/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
 # init.sh must be sourced to let it export env vars
-. ${PROJECT_DIR}/scripts/deploy/init.sh
+. "${PROJECT_DIR}"/scripts/deploy/init.sh
 
-sh ${PROJECT_DIR}/scripts/deploy/terraform.sh ${@}
+sh "${PROJECT_DIR}"/scripts/deploy/terraform.sh "${@}"
--- {{cookiecutter.project_dirname}}/scripts/deploy/vault.sh
+++ {{cookiecutter.project_dirname}}/scripts/deploy/vault.sh
@@ -1,14 +1,18 @@
 #!/bin/sh -e
 
-export VAULT_TOKEN=`curl --silent --request POST --data "role=${VAULT_ROLE}" --data "jwt=${CI_JOB_JWT_V2}" ${VAULT_ADDR%/}/v1/auth/gitlab-jwt/login | jq -r .auth.client_token`
+vault_token=$(curl --silent --request POST --data "role=${VAULT_ROLE}" --data "jwt=${CI_JOB_JWT_V2}" "${VAULT_ADDR%/}"/v1/auth/gitlab-jwt/login | jq -r .auth.client_token)
+
+secrets_data="{}"
 
 for secret_path in ${VAULT_SECRETS}
 do
-    secret_var_file=${TERRAFORM_VARS_DIR}/`echo ${secret_path} | tr / -`.json
-    curl --silent --header "X-Vault-Token: ${VAULT_TOKEN}" ${VAULT_ADDR%/}/v1/${PROJECT_SLUG}/${VAULT_SECRETS_PREFIX}/${secret_path} | jq -r ".data // {}" > ${secret_var_file}
-    var_files="${var_files} -var-file=${secret_var_file}"
+    secret_data=$(curl --silent --header "X-Vault-Token: ${vault_token}" "${VAULT_ADDR%/}"/v1/"${PROJECT_SLUG}"/"${VAULT_SECRETS_PREFIX}"/"${secret_path}" | jq -r '.data // {}') || secret_data="{}"
+    secrets_data=$(echo "${secrets_data}" | jq --argjson new_data "${secret_data}" '. * $new_data')
 done
 
-if [ "${TERRAFORM_BACKEND}" == "terraform-cloud" ]; then
-    export TFC_TOKEN=`curl --silent --header "X-Vault-Token: ${VAULT_TOKEN}" ${VAULT_ADDR%/}/v1/${PROJECT_SLUG}-tfc/creds/default | jq -r .data.token `
+echo "${secrets_data}" > "${TERRAFORM_VARS_DIR%/}"/vault-secrets.tfvars.json
+
+if [ "${TERRAFORM_BACKEND}" = "terraform-cloud" ]; then
+    TFC_TOKEN=$(curl --silent --header "X-Vault-Token: ${vault_token}" "${VAULT_ADDR%/}"/v1/"${PROJECT_SLUG}"-tfc/creds/default | jq -r .data.token)
+    export TFC_TOKEN
 fi
