77f31732407075bd0987ec51ca61815115f95ff8
Filippo Morelli <filippo@20tab.com>
2023-02-13 19:08:34 +0100

    TG-921 TG-925 Refactoring sh scripts (#230)

--- {{cookiecutter.project_dirname}}/scripts/deploy/init.sh
+++ {{cookiecutter.project_dirname}}/scripts/deploy/init.sh
@@ -4,27 +4,27 @@ export TF_VAR_env_slug="${ENV_SLUG}"
 export TF_VAR_project_slug="${PROJECT_SLUG}"
 export TF_VAR_stack_slug="${STACK_SLUG}"
 
-var_file="${TERRAFORM_VARS_DIR}/.tfvars"
+terraform_cli_args="-var-file=${TERRAFORM_VARS_DIR%/}/.tfvars"
 
 if [ "${TERRAFORM_EXTRA_VAR_FILE}" != "" ]; then
-  extra_var_file="${TERRAFORM_VARS_DIR}/${TERRAFORM_EXTRA_VAR_FILE}"
-  touch ${extra_var_file}
-  var_files="-var-file=${var_file} -var-file=${extra_var_file}"
-else
-  var_files="-var-file=${var_file}"
+  extra_var_file="${TERRAFORM_VARS_DIR%/}/${TERRAFORM_EXTRA_VAR_FILE}"
+  touch "${extra_var_file}"
+  terraform_cli_args="${terraform_cli_args} -var-file=${extra_var_file}"
 fi
 
 if [ "${VAULT_ADDR}" != "" ]; then
-  . ${PROJECT_DIR}/scripts/deploy/vault.sh
+  . "${PROJECT_DIR}"/scripts/deploy/vault.sh
+  terraform_cli_args="${terraform_cli_args} -var-file=${TERRAFORM_VARS_DIR%/}/vault-secrets.tfvars.json"
 fi
 
-export TERRAFORM_VAR_FILE_ARGS=${var_files}
+export TF_CLI_ARGS_destroy="${terraform_cli_args}"
+export TF_CLI_ARGS_plan="${terraform_cli_args}"
 
 case "${TERRAFORM_BACKEND}" in
   "gitlab")
-    . ${PROJECT_DIR}/scripts/deploy/gitlab.sh
+    . "${PROJECT_DIR}"/scripts/deploy/gitlab.sh
   ;;
   "terraform-cloud")
-    . ${PROJECT_DIR}/scripts/deploy/terraform-cloud.sh
+    . "${PROJECT_DIR}"/scripts/deploy/terraform-cloud.sh
   ;;
 esac
