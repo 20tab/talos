f2d292d23ea55fd3cd1b883fcbdda6fb7994e98e
Matteo Vitali <matteo@20tab.com>
2023-04-21 20:21:39 +0200

    Restore check failure

--- {{cookiecutter.project_dirname}}/terraform/environment/other-k8s/main.tf
+++ {{cookiecutter.project_dirname}}/terraform/environment/other-k8s/main.tf
@@ -100,15 +100,6 @@ module "monitoring" {
   s3_host        = var.s3_host
 }
 
-
-/* Metrics */
-
-module "metrics" {
-  count = var.stack_slug == "main" && var.env_slug == "prod" ? 1 : 0
-
-  source = "../modules/kubernetes/metrics"
-}
-
 /* Routing */
 
 module "routing" {
